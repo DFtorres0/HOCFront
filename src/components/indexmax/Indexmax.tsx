a957a5f88d7222f651aecc5e2c55eeb242d9c93b
Mafe <mariaferco94@gmail.com>
2023-03-15 02:34:02 -0500

    NavegationMax added

--- src/components/indexmax/Indexmax.tsx
+++ src/components/indexmax/Indexmax.tsx
@@ -3,12 +3,14 @@ import { Link } from "react-router-dom";
 import {Container, Button } from "react-bootstrap";
 import Headermax from "./headermax";
 import Aboutmax from "./aboutmax";
+import NavegationMax from "./NavegationMax";
 
 
 function Indexmax() {
 
   return (
     <div>
+      <NavegationMax/>
       <Headermax/>
       <Aboutmax/>
     </div>
---
+++ src/components/indexmax/NavegationMax.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { Navbar, Container,Nav, ListGroup } from "react-bootstrap";
+import "../../assets/styles/indexmax/indexmaxS.css";
+import { BsCodeSlash } from "react-icons/bs";
+
+const NavigationMax = () => {
+  const [navbarClass, setNavbarClass] = useState('transparent');
+
+  const handleScroll = () => {
+    const currentPosition = window.pageYOffset;
+    if (currentPosition > 0 && navbarClass === 'transparent') {
+      setNavbarClass('colored');
+    } else if (currentPosition === 0 && navbarClass === 'colored') {
+      setNavbarClass('transparent');
+    }
+  };
+
+  window.addEventListener('scroll', handleScroll);
+  return (
+    <Navbar
+        className={navbarClass}
+        fixed="top"
+        bg="transparent"
+        variant="dark"
+      >
+        <Container>
+          <Navbar.Brand href="/home">Home of Code</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav"></Navbar.Collapse>
+          <Nav className="ml-auto">
+            <Nav.Link href="#features" className="Header-NavbarLink">
+              Inicio
+            </Nav.Link>
+            <Nav.Link href="#features" className="Header-NavbarLink">
+              Clases
+            </Nav.Link>
+            <Nav.Link href="#features" className="Header-NavbarLink">
+              Foros
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+  );
+};
+
+export default NavigationMax;
