348a61555984ee7c8585538f535dca6d6fdba782
DFtorres <dftorres2004@gmail.com>
2023-07-29 14:51:07 -0500

    deleted unused files

--- src/core/services/MainCourse.service.ts
+++
@@ -1,9 +0,0 @@
-import { Console } from "console";
-import { enviroment } from "../../enviroments/enviroment";
-import { User } from "../models/User";
-import { UserRole } from "../models/UserRole";
-
-export class MainCourseService {
-  private baseUrl = `${enviroment.apiUrl}/Users/`;
-
-}
