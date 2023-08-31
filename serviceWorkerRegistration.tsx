37df4d35869173bcfd23ef815c8d55e3bbd99e7e
Andresjl389 <andresjaimes195@gmail.com>
2023-08-31 11:54:14 -0500

    PWA

---
+++ serviceWorkerRegistration.tsx
@@ -0,0 +1,19 @@
+export function register() {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+        console.log('ServiceWorker registrado con éxito:', registration);
+      }).catch((error) => {
+        console.log('Error al registrar el ServiceWorker:', error);
+      });
+    });
+  }
+}
+
+export function unregister() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready.then((registration) => {
+      registration.unregister();
+    });
+  }
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { UserService } from "./core/services/User.service";
+import * as serviceWorkerRegistration from "../serviceWorkerRegistration"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
@@ -13,3 +14,13 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 const userService = new UserService();
 const response = await userService.getAll()
 console.log(response)
+
+
+if ("service-worker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/service-worker.ts")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
+}
